e211400d7340def6e1754a7f21eaf601943e5c27
dpathim <dpdevops6@gmail.com>
2023-08-07 18:35:06 -0400

    Roboshop Scripts updated DNS Records.

--- catalogue.sh
+++ catalogue.sh
@@ -1,4 +1,4 @@
-cp catalogue.service /etc/systemd/system/catalogue.sh
+cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
