a0dbbea95a2c3d8f58b85b4bb31956e224924067
dpathim <dpdevops6@gmail.com>
2023-08-15 18:31:41 -0400

    Exit Status.if condition.

--- common.sh
+++ common.sh
@@ -1,10 +1,10 @@
 log=/tmp/roboshop.log
 # func_exit_status if condition
 func_exit_status() {
-  if [ $? -eq 0 ];then
+  if [ $? -eq 0 ]; then
      echo -e "\e[32m SUCCESS \e[0m"
     else
-      echo-e "\e[31m FAILURE \e[0m"
+      echo -e "\e[31m FAILURE \e[0m"
      fi
 }
 func_apppreq() {
