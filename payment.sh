6a3411e16acd8ddda4b310f54a4bae7a68522e6e
dpathim <dpdevops6@gmail.com>
2023-08-21 14:49:57 -0400

    Password NFR

--- payment.sh
+++ payment.sh
@@ -1,6 +1,6 @@
 component=payment
 source common.sh
-rabbitmq_app_password =$1
+rabbitmq_app_password=$1
 if [ -z "${rabbitmq_app_password}" ]; then
   echo Input RabbitMQ AppUser password Missing
   exit 1
